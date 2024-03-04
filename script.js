13c8b0a8818f0b1fa63de40593fe01e0e5e9e1e9
realmbs <119774290+realmbs@users.noreply.github.com>
2024-03-03 18:16:36 -0600

    gamelogic......

--- script.js
+++ script.js
@@ -1,7 +1,80 @@
 var cells = document.querySelectorAll('.cell');
-var board = document.querySelector('.container');
+var container = document.querySelector('.container');
 cells.forEach(function (cell) {
     cell.addEventListener('click', function () {
         cell.classList.toggle('active');
     });
 });
+var Gameboard = /** @class */ (function () {
+    function Gameboard(rows, cols) {
+        if (rows === void 0) { rows = 3; }
+        if (cols === void 0) { cols = 3; }
+        this.rows = rows;
+        this.cols = cols;
+    }
+    Gameboard.prototype.generateGameboard = function () {
+        var cellCount = this.rows * this.cols;
+        for (var i = 0; i < cellCount; i++) {
+            var cell = document.createElement('div');
+            cell.classList.add('cell');
+            container === null || container === void 0 ? void 0 : container.appendChild(cell);
+            cell.id = "".concat(i);
+        }
+    };
+    Gameboard.prototype.clearGameboard = function () {
+        container === null || container === void 0 ? void 0 : container.innerHTML = '';
+    };
+    Gameboard.prototype.generateWinningConditions = function () {
+        var winningConditions = [];
+        // Rows
+        for (var i = 0; i < this.rows; i++) {
+            var row = [];
+            for (var j = 0; j < this.cols; j++) {
+                row.push(i * this.cols + j);
+            }
+            winningConditions.push(row);
+        }
+        // Columns
+        for (var i = 0; i < this.cols; i++) {
+            var col = [];
+            for (var j = 0; j < this.rows; j++) {
+                col.push(j * this.cols + i);
+            }
+            winningConditions.push(col);
+        }
+        // Diagonals
+        var diagonal1 = [];
+        var diagonal2 = [];
+        for (var i = 0; i < this.rows; i++) {
+            diagonal1.push(i * this.cols + i);
+            diagonal2.push(i * this.cols + this.cols - 1 - i);
+        }
+        winningConditions.push(diagonal1);
+        winningConditions.push(diagonal2);
+        return winningConditions;
+    };
+    return Gameboard;
+}());
+var Player = /** @class */ (function () {
+    function Player(name, isComputer) {
+        if (isComputer === void 0) { isComputer = false; }
+        this.name = name;
+        this.isComputer = isComputer;
+    }
+    Player.prototype.makeMove = function (cell) {
+        cell.classList.add('active');
+    };
+    Player.prototype.makeComputerMove = function () {
+        var cells = document.querySelectorAll('.cell');
+        var emptyCells = Array.from(cells).filter(function (cell) { return !cell.classList.contains('active'); });
+        var randomIndex = Math.floor(Math.random() * emptyCells.length);
+        var randomCell = emptyCells[randomIndex];
+        randomCell.classList.add('active');
+    };
+    return Player;
+}());
+var gameboard = new Gameboard();
+gameboard.generateGameboard();
+var playerOne = new Player('Wade Watts', false);
+var playerTwo = new Player('Art3mis', true);
+playerOne.makeMove(document.getElementById('0'));
--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 const cells = document.querySelectorAll('.cell');
-const board = document.querySelector('.container');
+const container = document.querySelector('.container') as HTMLDivElement;
 
 cells.forEach(cell => {
   cell.addEventListener('click', () => {
@@ -7,3 +7,87 @@ cells.forEach(cell => {
   });
 });
 
+class Gameboard {
+  rows: number;
+  cols: number;
+
+  constructor(rows: number = 3, cols: number = 3) {
+    this.rows = rows;
+    this.cols = cols;
+  }
+
+  generateGameboard() {
+    const cellCount = this.rows * this.cols;
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      container?.appendChild(cell);
+      cell.id = `${i}`;
+    }
+  }
+
+  clearGameboard() {
+    container?.innerHTML = '';
+  }
+
+  generateWinningConditions() {
+    const winningConditions = [];
+    // Rows
+    for (let i = 0; i < this.rows; i++) {
+      const row = [];
+      for (let j = 0; j < this.cols; j++) {
+        row.push(i * this.cols + j);
+      }
+      winningConditions.push(row);
+    }
+    // Columns
+    for (let i = 0; i < this.cols; i++) {
+      const col = [];
+      for (let j = 0; j < this.rows; j++) {
+        col.push(j * this.cols + i);
+      }
+      winningConditions.push(col);
+    }
+    // Diagonals
+    const diagonal1 = [];
+    const diagonal2 = [];
+    for (let i = 0; i < this.rows; i++) {
+      diagonal1.push(i * this.cols + i);
+      diagonal2.push(i * this.cols + this.cols - 1 - i);
+    }
+    winningConditions.push(diagonal1);
+    winningConditions.push(diagonal2);
+    return winningConditions;
+  }
+}
+
+class Player {
+  name: string;
+  isComputer: boolean;
+  constructor(name: string, isComputer: boolean = false) {
+    this.name = name;
+    this.isComputer = isComputer;
+  }
+
+  makeMove(cell: HTMLDivElement) {
+    cell.classList.add('active');
+  }
+
+  makeComputerMove() {
+    const cells = document.querySelectorAll('.cell');
+    const emptyCells = Array.from(cells).filter(cell => !cell.classList.contains('active'));
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const randomCell = emptyCells[randomIndex] as HTMLDivElement;
+    randomCell.classList.add('active');
+  }
+}
+
+
+
+
+const gameboard = new Gameboard();
+gameboard.generateGameboard();
+
+const playerOne = new Player('Wade Watts', false);
+const playerTwo = new Player('Art3mis', true);
+playerOne.makeMove(document.getElementById('0') as HTMLDivElement);
